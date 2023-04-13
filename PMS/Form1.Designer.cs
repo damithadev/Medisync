d0478fe66ed2943722ee4a155268e254abc96598
Damitha <hellodamitha@gmail.com>
2023-04-13 15:13:10 +0530

    3rd commit

--- PMS/Form1.Designer.cs
+++ PMS/Form1.Designer.cs
@@ -28,48 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.pnl_sidebar = new System.Windows.Forms.Panel();
             this.pnl_logo = new System.Windows.Forms.Panel();
             this.pnl_top = new System.Windows.Forms.Panel();
             this.pnl_body = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.pnl_sidebar.SuspendLayout();
+            this.pnl_logo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pnl_sidebar
             // 
-            this.pnl_sidebar.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.pnl_sidebar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(140)))), ((int)(((byte)(228)))));
             this.pnl_sidebar.Controls.Add(this.pnl_logo);
             this.pnl_sidebar.Dock = System.Windows.Forms.DockStyle.Left;
             this.pnl_sidebar.Location = new System.Drawing.Point(0, 0);
             this.pnl_sidebar.Name = "pnl_sidebar";
-            this.pnl_sidebar.Size = new System.Drawing.Size(249, 663);
+            this.pnl_sidebar.Size = new System.Drawing.Size(261, 663);
             this.pnl_sidebar.TabIndex = 0;
             // 
             // pnl_logo
             // 
-            this.pnl_logo.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.pnl_logo.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.pnl_logo.Controls.Add(this.label1);
+            this.pnl_logo.Controls.Add(this.pictureBox1);
             this.pnl_logo.Dock = System.Windows.Forms.DockStyle.Top;
             this.pnl_logo.Location = new System.Drawing.Point(0, 0);
             this.pnl_logo.Name = "pnl_logo";
-            this.pnl_logo.Size = new System.Drawing.Size(249, 143);
+            this.pnl_logo.Size = new System.Drawing.Size(261, 143);
             this.pnl_logo.TabIndex = 0;
             // 
             // pnl_top
             // 
-            this.pnl_top.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.pnl_top.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(140)))), ((int)(((byte)(228)))));
             this.pnl_top.Dock = System.Windows.Forms.DockStyle.Top;
-            this.pnl_top.Location = new System.Drawing.Point(249, 0);
+            this.pnl_top.Location = new System.Drawing.Point(261, 0);
             this.pnl_top.Name = "pnl_top";
-            this.pnl_top.Size = new System.Drawing.Size(904, 43);
+            this.pnl_top.Size = new System.Drawing.Size(892, 43);
             this.pnl_top.TabIndex = 1;
             // 
             // pnl_body
             // 
+            this.pnl_body.BackColor = System.Drawing.SystemColors.HighlightText;
             this.pnl_body.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pnl_body.Location = new System.Drawing.Point(249, 43);
+            this.pnl_body.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pnl_body.Location = new System.Drawing.Point(261, 43);
             this.pnl_body.Name = "pnl_body";
-            this.pnl_body.Size = new System.Drawing.Size(904, 620);
+            this.pnl_body.Size = new System.Drawing.Size(892, 620);
             this.pnl_body.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(86, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(76, 70);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Dubai Medium", 8F);
+            this.label1.Location = new System.Drawing.Point(39, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(178, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Patient Management System";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
             // 
             // Form1
             // 
@@ -79,10 +110,14 @@
             this.Controls.Add(this.pnl_body);
             this.Controls.Add(this.pnl_top);
             this.Controls.Add(this.pnl_sidebar);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Patient Management System";
             this.pnl_sidebar.ResumeLayout(false);
+            this.pnl_logo.ResumeLayout(false);
+            this.pnl_logo.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -93,6 +128,8 @@
         private System.Windows.Forms.Panel pnl_logo;
         private System.Windows.Forms.Panel pnl_top;
         private System.Windows.Forms.Panel pnl_body;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- PMS/Form1.cs
+++ PMS/Form1.cs
@@ -21,5 +21,15 @@ namespace PMS
         {
 
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
     }
 }
