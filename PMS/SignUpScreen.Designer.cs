1171a11a2f4ff17dac3150b9e8b0000d6c4a7153
Damitha <hellodamitha@gmail.com>
2023-05-01 23:02:02 +0530

    gh

--- PMS/SignUpScreen.Designer.cs
+++ PMS/SignUpScreen.Designer.cs
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // SignUpScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "SignUpScreen";
             this.Text = "SignUpScreen";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
