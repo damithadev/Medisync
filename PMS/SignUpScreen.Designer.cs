d9a32dc9514f433967b0aefeada5ae7e981cfde0
Damitha <hellodamitha@gmail.com>
2023-05-02 00:08:17 +0530

    gh

--- PMS/SignUpScreen.Designer.cs
+++ PMS/SignUpScreen.Designer.cs
@@ -28,24 +28,125 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.NIC = new System.Windows.Forms.Label();
+            this.Lname = new System.Windows.Forms.Label();
+            this.Fname = new System.Windows.Forms.Label();
+            this.password = new System.Windows.Forms.Label();
+            this.rePassword = new System.Windows.Forms.Label();
+            this.signUpBtn = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // label1
+            // NIC
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(438, 144);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 16);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.NIC.AutoSize = true;
+            this.NIC.Location = new System.Drawing.Point(438, 147);
+            this.NIC.Name = "NIC";
+            this.NIC.Size = new System.Drawing.Size(29, 16);
+            this.NIC.TabIndex = 0;
+            this.NIC.Text = "NIC";
+            this.NIC.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Lname
+            // 
+            this.Lname.AutoSize = true;
+            this.Lname.Location = new System.Drawing.Point(438, 259);
+            this.Lname.Name = "Lname";
+            this.Lname.Size = new System.Drawing.Size(72, 16);
+            this.Lname.TabIndex = 1;
+            this.Lname.Text = "Last Name";
+            // 
+            // Fname
+            // 
+            this.Fname.AutoSize = true;
+            this.Fname.Location = new System.Drawing.Point(438, 202);
+            this.Fname.Name = "Fname";
+            this.Fname.Size = new System.Drawing.Size(72, 16);
+            this.Fname.TabIndex = 2;
+            this.Fname.Text = "First Name";
+            // 
+            // password
+            // 
+            this.password.AutoSize = true;
+            this.password.Location = new System.Drawing.Point(438, 312);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(67, 16);
+            this.password.TabIndex = 3;
+            this.password.Text = "Password";
+            // 
+            // rePassword
+            // 
+            this.rePassword.AutoSize = true;
+            this.rePassword.Location = new System.Drawing.Point(438, 367);
+            this.rePassword.Name = "rePassword";
+            this.rePassword.Size = new System.Drawing.Size(118, 16);
+            this.rePassword.TabIndex = 4;
+            this.rePassword.Text = "Re-type Password";
+            // 
+            // signUpBtn
+            // 
+            this.signUpBtn.Location = new System.Drawing.Point(601, 455);
+            this.signUpBtn.Name = "signUpBtn";
+            this.signUpBtn.Size = new System.Drawing.Size(81, 30);
+            this.signUpBtn.TabIndex = 5;
+            this.signUpBtn.Text = "SignUp";
+            this.signUpBtn.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(601, 147);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(153, 22);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(601, 202);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(153, 22);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(601, 256);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(153, 22);
+            this.textBox3.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(601, 306);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(153, 22);
+            this.textBox4.TabIndex = 9;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(601, 367);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(153, 22);
+            this.textBox5.TabIndex = 10;
             // 
             // SignUpScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1064, 619);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.signUpBtn);
+            this.Controls.Add(this.rePassword);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.Fname);
+            this.Controls.Add(this.Lname);
+            this.Controls.Add(this.NIC);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "SignUpScreen";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -57,6 +158,16 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label NIC;
+        private System.Windows.Forms.Label Lname;
+        private System.Windows.Forms.Label Fname;
+        private System.Windows.Forms.Label password;
+        private System.Windows.Forms.Label rePassword;
+        private System.Windows.Forms.Button signUpBtn;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
     }
 }
