bad749319697002e8da83288ce116c94760848d0
Damitha <hellodamitha@gmail.com>
2023-05-07 22:18:32 +0530

    fg

--- PMS/SignUpScreen.cs
+++ PMS/SignUpScreen.cs
@@ -93,28 +93,7 @@ namespace PMS
                 }
 
 
-                    /* try
-                    {
-                        MySqlConnection connection = new MySqlConnection(functions.connectionString);
-                        string query = "INSERT INTO userTable (fname, lname, email, password) VALUES ('" + fname + "','" + lname + "','" + email + "','" + password + "')";
-                        MySqlCommand command = new MySqlCommand(query, connection);
-                        connection.Open();
-                        command.ExecuteNonQuery();
-                        new PopupMessage("Account registered successfully! Please use username and password to login..").ShowDialog();
-                        connection.Close();
-                    }
-                    catch (Exception ex)
-                    {
-                        new PopupMessage("Error registering account!" + ex).ShowDialog();
-                    }
-                    finally
-                    {
-                        textBoxfname.Text = "";
-                        textBoxlname.Text = "";
-                        textBoxemail.Text = "";
-                        textBoxpassword1.Text = "";
-                        textBoxpassword2.Text = "";
-                        }*/
+               
                  
             }
             else
