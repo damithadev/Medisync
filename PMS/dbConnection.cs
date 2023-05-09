4eca2902ea023141013466be2809c24768bc061e
Damitha <hellodamitha@gmail.com>
2023-05-09 12:36:06 +0530

    sa

--- PMS/dbConnection.cs
+++ PMS/dbConnection.cs
@@ -13,25 +13,6 @@ namespace PMS
     {
         public string connectionString = "server=139.59.62.182;port=3306;userid=damitha99;password=20020502;database=pms_db;sslmode=none;";
 
-        /*public int checkUserTaken(string email)
-        {
-            MySqlConnection connection = new MySqlConnection(connectionString);
-            string query = "SELECT COUNT(*) FROM userTable WHERE email= '" + email + "'";
-            MySqlCommand command = new MySqlCommand(query, connection);
-
-            connection.Open();
-            int reservationExist = (int)command.ExecuteScalar();
-            connection.Close();
-
-            if (reservationExist > 0)
-            {
-                return 0;
-            }
-            else
-            {
-                return 1;
-            }
-        }*/
 
 
         public int UserTaken(string email)
